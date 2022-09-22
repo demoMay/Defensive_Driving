0e28e31944f2b769341d377e41e67a26fe76918b
demoMay <105841692+demoMay@users.noreply.github.com>
2022-09-22 12:59:15 +0530

    22 Sep Build 2022
    22SepBuild

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,22 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6n51C0yxPkf":
+        Script1();
+        break;
+      case "6r0UDIBys1W":
+        Script2();
+        break;
   }
+}
+
+function Script1()
+{
+  window.print();
+}
+
+function Script2()
+{
+  window.print();
 }
 
